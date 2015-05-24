4f1ef8567ffa1f613b6d2283cbafe01abe1dbff3
Nitish Janardan <ugrashrath@gmail.com>
2015-05-23 21:23:03 -0500

    Fixed bloody shear damage

--- src/main/java/beneficialblocks/handler/EntityEventHandler.java
+++ src/main/java/beneficialblocks/handler/EntityEventHandler.java
@@ -1,15 +1,18 @@
 package beneficialblocks.handler;
 
+import beneficialblocks.BeneficialBlocks;
 import beneficialblocks.integration.MFRItems;
 import beneficialblocks.setup.ModItems;
 import net.minecraft.entity.EntityLiving;
 import net.minecraft.entity.item.EntityItem;
+import net.minecraft.entity.monster.EntityCreeper;
 import net.minecraft.entity.passive.EntityChicken;
 import net.minecraft.entity.passive.EntityCow;
 import net.minecraft.entity.passive.EntityMooshroom;
 import net.minecraft.entity.passive.EntityVillager;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.InventoryPlayer;
+import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.DamageSource;
@@ -99,11 +102,15 @@ public class EntityEventHandler
 					if(invoStack.stackSize <= 1)
 					{
 						inv.setInventorySlotContents(i, new ItemStack(Items.milk_bucket));
+						BeneficialBlocks.bbLog.info("Index of no stack: " + i);
+						break;
 					}
 					else if (inv.addItemStackToInventory(new ItemStack(Items.milk_bucket)))
 					{
 						invoStack.stackSize--;
 						inv.setInventorySlotContents(i, invoStack);
+						BeneficialBlocks.bbLog.info("Index of stack: " + i);
+						break;
 					}
 				}
 				else if(invoStack.getItem() == Items.glass_bottle)
@@ -135,7 +142,7 @@ public class EntityEventHandler
 				{
 					w.spawnEntityInWorld(new EntityItem(w, ev.target.posX, ev.target.posY, ev.target.posZ, new ItemStack(Items.emerald)));
 				}
-				inv.getCurrentItem().setItemDamage(inv.getCurrentItem().getItemDamage());
+				inv.getCurrentItem().setItemDamage(inv.getCurrentItem().getItemDamage()+1);
 			}
 		}
 		
@@ -153,7 +160,7 @@ public class EntityEventHandler
 				{
 					w.spawnEntityInWorld(new EntityItem(w, ev.target.posX, ev.target.posY, ev.target.posZ, new ItemStack(Items.feather)));
 				}
-				inv.getCurrentItem().setItemDamage(inv.getCurrentItem().getItemDamage());					
+				inv.getCurrentItem().setItemDamage(inv.getCurrentItem().getItemDamage()+1);					
 			}
 		}
 		
@@ -171,8 +178,26 @@ public class EntityEventHandler
 				{
 					w.spawnEntityInWorld(new EntityItem(w, ev.target.posX, ev.target.posY, ev.target.posZ, new ItemStack(Items.leather, 2)));
 				}
-				inv.getCurrentItem().setItemDamage(inv.getCurrentItem().getItemDamage());				
+				inv.getCurrentItem().setItemDamage(inv.getCurrentItem().getItemDamage()+1);				
 			}
-		}		
+		}
+		
+		/*
+		 * 
+		 * Extracts TNT from Creeper
+		 * 
+		 * */
+		if(ev.target instanceof EntityCreeper && p.isSneaking() && inv.getCurrentItem().getItem() == ModItems.bloodyShears)
+		{
+			if(((EntityLiving)ev.target).getHealth()>= 10)
+			{
+				ev.target.attackEntityFrom(bloodyShears, 10);
+				if(!w.isRemote)
+				{
+					w.spawnEntityInWorld(new EntityItem(w, ev.target.posX, ev.target.posY, ev.target.posZ, new ItemStack(Blocks.tnt)));
+				}
+				inv.getCurrentItem().setItemDamage(inv.getCurrentItem().getItemDamage()+1);				
+			}
+		}
 	}
 }
