97c2d0faa4af4afe17c10bbea5a35fad3d18952c
Nitish Janardan <ugrashrath@gmail.com>
2015-05-24 21:20:18 -0500

    Added block recipes, updated recipes

--- src/main/java/beneficialblocks/crafting/Recipies.java
+++ src/main/java/beneficialblocks/crafting/Recipies.java
@@ -47,10 +47,37 @@ public class Recipies
 			"BN ",
 			'N', Items.netherbrick, 'R', Items.blaze_rod, 'P', Items.blaze_powder, 'B', Blocks.iron_block
 		});
+		
+		GameRegistry.addRecipe(new ItemStack(ModItems.gelidProiectum), new Object[]
+		{
+			"BSG", 
+			"CDC", 
+			"C C",
+			'B', Items.water_bucket, 'S', Items.snowball, 'G', Blocks.glass, 'C', Blocks.clay, 'D', Items.diamond
+		});
+		
+		GameRegistry.addRecipe(new ItemStack(ModItems.ignisProiectum), new Object[]
+		{
+			"BFG", 
+			"CDC", 
+			"C C",
+			'B', Items.lava_bucket, 'F', Items.fire_charge, 'G', Blocks.glass, 'C', Blocks.clay, 'D', Items.diamond
+		});
 	}
 		
 	public static void makeBlocks()
 	{
+		GameRegistry.addRecipe(new ItemStack(ModItems.glowingLeaves), new Object[]
+		{
+			" D ", 
+			"DLD", 
+			" D ", 
+			'D', Items.glowstone_dust, 'L', Blocks.leaves 
+		});
 		
+		GameRegistry.addRecipe(new ItemStack(ModItems.hopperChest), new Object[]
+		{
+			"CHC", 'C', Blocks.chest, 'H', Blocks.hopper
+		});
 	}
 }
