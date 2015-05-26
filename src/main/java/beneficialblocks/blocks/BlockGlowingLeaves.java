e295f841d441d35d90debc41bd11cdf99f4c3f7f
Nitish Janardan <ugrashrath@gmail.com>
2015-05-25 23:46:49 -0500

    Made leaves drop glowstone dust instead of themselves

--- src/main/java/beneficialblocks/blocks/BlockGlowingLeaves.java
+++ src/main/java/beneficialblocks/blocks/BlockGlowingLeaves.java
@@ -1,8 +1,12 @@
 package beneficialblocks.blocks;
 
+import java.util.Random;
+
 import beneficialblocks.setup.CreativeTabBB;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.init.Items;
+import net.minecraft.item.Item;
 
 public class BlockGlowingLeaves extends Block
 {
@@ -13,11 +17,22 @@ public class BlockGlowingLeaves extends Block
 		setBlockTextureName("beneficialblocks:GlowingLeaves");
 		setCreativeTab(CreativeTabBB.BB_TAB);
 		setStepSound(soundTypeGlass);
-		this.setLightLevel(1.0F);
+		setLightLevel(1.0F);
+		setHardness(0.3F);
 	}
 	
     public boolean isOpaqueCube()
     {
         return false;
+    }
+    
+    public int quantityDropped(Random r)
+    {
+        return 4;
+    }
+
+    public Item getItemDropped(int i1, Random r, int i2)
+    {
+        return Items.glowstone_dust;
     }
 }
